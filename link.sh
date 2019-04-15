b665c3d981b17498cb2e321c5eb322f56445862a
tsukker <stutaw+github@gmail.com>
2019-04-16 03:40:50 +0900

    Add .vim directory

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 # ln -sf [path of the real file (if relative from symbolic link)] [path of symbolic link (if relative from working directory]
 
-dotfiles=".tmux.conf .vimrc .zshenv .zshrc"
+dotfiles=".tmux.conf .vim .vimrc .zshenv .zshrc"
 for dotfile in $dotfiles
 do
     ln -sf ~/dotfiles/$dotfile ~/$dotfile

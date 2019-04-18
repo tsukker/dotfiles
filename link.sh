57dc3a0a856a24d2bcaa07c4e40db172b8ab1ca9
tsukker <tsukker@users.noreply.github.com>
2019-04-18 16:01:17 +0900

    Add nvim config init.vim
    Vim -> Neovim

--- link.sh
+++ link.sh
@@ -1,7 +1,18 @@
+#!/bin/sh
+
+# ln cheat-sheet
 # ln -sf [path of the real file (if relative from symbolic link)] [path of symbolic link (if relative from working directory]
 
+# dotfiles under home directory
 dotfiles=".tmux.conf .vim .vimrc .zshenv .zshrc"
 for dotfile in $dotfiles
 do
-    ln -sf ~/dotfiles/$dotfile ~/$dotfile
+  ln -sf ~/dotfiles/$dotfile ~/
+done
+
+# XDG Base Directory Specification
+configdirs="nvim"
+for dir in $configdirs
+do
+  ln -sf ~/dotfiles/.config/$dir ~/.config/
 done

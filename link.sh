303f00c9a50dc4fddd29ebcd31fab72319406896
tsukker <stutaw+github@gmail.com>
2019-04-25 03:11:51 +0900

    Add .editorconfig & Fix link.sh

--- link.sh
+++ link.sh
@@ -4,8 +4,8 @@
 # ln -sf [path of the real file (if relative from symbolic link)] [path of symbolic link (if relative from working directory]
 
 # dotfiles under home directory
-dotfiles=".tmux.conf .vim .vimrc .zshenv .zshrc"
-for dotfile in $dotfiles
+dotfiles=".editorconfig .tmux.conf .vim .vimrc .zshenv .zshrc"
+for dotfile in $(echo $dotfiles)
 do
   ln -sf ~/dotfiles/$dotfile ~/
 done
